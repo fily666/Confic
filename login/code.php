55525c4a9aca21e17d31d071daf605fc3cf313ef
unknown <wilmarfelipermail.con>
2018-11-08 18:50:28 -0500

    guardado con alert

--- login/code.php
+++ login/code.php
@@ -9,13 +9,32 @@
 
         $conexion = mysqli_connect("localhost","root","","confic");
         $consulta = "INSERT INTO user(name,username,password) VALUE('$name','$username','$password')";
-        $resultado = mysqli_query($conexion, $consulta);
        
-        if(! $resultado){
-                echo'error al registrar'; 
-              } 
-        echo "¡gracias! hemos recivido sus datos";
 
+        $verificar_username = mysqli_query($conexion, "SELECT * FROM user WHERE  username = '$username'");
+     if(mysqli_num_rows($verificar_username) > 0){
+             echo '<script>
+             alert("El Usuario ya esta Registrado");
+             window.history.go(-1);
+             </script>';
+             exit;
+     }
+        
+ $resultado = mysqli_query($conexion, $consulta);
+        if(!$resultado){
+                echo'<script>
+             alert("Error al Registrar");
+             window.history.go(-1);
+             </script>'; 
+              } 
+       else{
+            echo '<script>
+            alert("Usuario  Registrado Exitosamente");
+            window.history.go(-1);
+            </script>';    
+       }
         mysqli_close($conexion);
+
+
 
         ?>
